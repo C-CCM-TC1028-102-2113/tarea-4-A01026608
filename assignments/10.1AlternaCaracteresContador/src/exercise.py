e667b0609a1b13bff4788636a66333c912aabda6
A01026608 <89270787+A01026608@users.noreply.github.com>
2021-09-06 10:25:55 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
+    num= int(input())
+    for numero in range (num):
+        if numero%2==0:
+            print('#')
+        else:
+            print('%')
 if __name__=='__main__':   
     main()
