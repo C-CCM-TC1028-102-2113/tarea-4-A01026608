31483d4573d0c2b20db7c98f035d5cba86e5ad05
A01026608 <89270787+A01026608@users.noreply.github.com>
2021-09-07 19:47:37 -0500

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,8 +1,13 @@
 
 def main():
     #Escribe tu código debajo de esta línea
-    pass
-
-
+    def trian(h):
+        for i in range (h+1):
+            e= h-i# los espacios 
+            print(' '*e+'*'*i)
+    
+    h= int(input('Dame la altura de el trianlgulo:'))
+    trian(h)
+    
 if __name__=='__main__':
     main()
